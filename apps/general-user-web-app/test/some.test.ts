94a56653deeb5d2e160994e65f25bd46ecb5a910
hbenpitsu <hbenpitsu15@gmail.com>
2026-09-04 09:56:37 +0000

    スクリプトを整理

--- apps/general-user-web-app/test/some.test.ts
+++
@@ -1,7 +0,0 @@
-import { describe, it, expect } from "vitest";
-
-describe("example test", () => {
-  it("should executed", () => {
-    expect(true).toBe(true);
-  });
-});
--- apps/organization-content-service/src/index.ts
+++
@@ -1,9 +0,0 @@
-import { Hono } from "hono";
-
-const app = new Hono<{ Bindings: CloudflareBindings }>();
-
-app.get("/", (c) => {
-  return c.text(`Hello Hono! this is ${c.env.SELF}`);
-});
-
-export default app;
