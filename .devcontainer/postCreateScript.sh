1ac061ae9fe02797987e90876926484c0b21942f
hbenpitsu <hbenpitsu15@gmail.com>
2026-09-12 16:02:37 +0000

    postCreateScriptレビュー対応

--- .devcontainer/postCreateScript.sh
+++ .devcontainer/postCreateScript.sh
@@ -25,11 +25,12 @@ git config fetch.prune true
 # ---------------------------
 
 pnpm install --frozen-lockfile
-pnpm self-update
 pnpm prepare
 
 # ---------------------------
 # Wiki Repo Setup
 # ---------------------------
 
-git clone https://github.com/nu-shinkan-project/nu-shinkan.wiki.git
+if [[ ! -d "$target_dir" ]]; then
+  git clone --depth 1 https://github.com/nu-shinkan-project/nu-shinkan.wiki.git
+fi
